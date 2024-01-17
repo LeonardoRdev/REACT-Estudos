f3ecf818b2eef2f4c4b6546e809db86ce2fb0691
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-01-17 16:55:15 -0300

    Componentes criados

---
+++ Primeira Aula de REACT/src/components/MeuComponente.jsx
@@ -0,0 +1,6 @@
+export default function MeuComponente() {
+    return (
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor excepturi quidem doloribus repellendus temporibus enim 
+        autem! Reprehenderit deleniti debitis est rerum soluta necessitatibus expedita odio impedit. Earum iure nisi nobis?</p>
+    )
+  }
